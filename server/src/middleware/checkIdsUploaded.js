754aea696a165b14df084ff2ee62b5d12c48d167
Mikyas Endalkachew <mikyasendalkachew037@gmail.com>
2025-12-20 15:13:20 +0300

    feat: added fayda and kebele schemas, and modified checkIdsUploaded.js

--- server/src/middleware/checkIdsUploaded.js
+++ server/src/middleware/checkIdsUploaded.js
@@ -1,11 +1,16 @@
 import UploadedID from "../models/UploadedID.js";
+import KebeleId from "../models/kebeleIdSchema.js";
+import FaydaId from "../models/faydaIdSchema.js";
 
 const checkIdsUploaded = async (req, res, next) => {
   const userId = req.user.id;
 
-  const ids = await UploadedID.find({ user: userId });
-  const hasFayda = ids.some((id) => id.type === "fayda");
-  const hasKebele = ids.some((id) => id.type === "kebele");
+  const hasFayda = FaydaId.findOne({ user: userId})
+  const hasKebele = KebeleId.findOne({ user: userId })
+
+  // const ids = await UploadedID.find({ user: userId });
+  // const hasFayda = ids.some((id) => id.type === "fayda");
+  // const hasKebele = ids.some((id) => id.type === "kebele");
 
   if (!hasKebele || !hasFayda) {
     return res.status(400).json({
